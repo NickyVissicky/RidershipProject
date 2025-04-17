18a9976ae41e84bbe708c615744f7aa251ba11b0
NickyVissicky <93458815+NickyVissicky@users.noreply.github.com>
2025-04-17 18:11:19 -0400

    Update RidershipProj.py

--- RidershipProj.py
+++ RidershipProj.py
@@ -22,6 +22,8 @@ ax.set_xlabel("Date")
 ax.set_ylabel("Total Riders")
 ax.set_title("Total Ridership Over Time")
 
+st.header("Total Ridership by Season")
+
 fig2, ax2 = plt.subplots()
 seasons = ['Winter', 'Spring', 'Summer', 'Fall']
 ax2.bar(seasons, df.groupby('season')['cnt'].sum())
@@ -29,3 +31,8 @@ ax2.set_xlabel("Season")
 ax2.set_ylabel("Total Riders")
 ax2.set_title("Total Ridership by Season")
 st.pyplot(fig2)
+
+st.header("Rolling Average of Riders (7d, 14d, Weekly)")
+option = st.radio(
+  "Select a Rolling Average",
+  ("7-Day Average, "14-Day Average", "Total by week")
